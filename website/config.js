ee84d1567b58f810dcb80faea42488551046dda2
LaurentDemanchelier <laurent@kepl.org>
2018-06-17 15:44:31 -0400

    inital commit

---
+++ website/config.js
@@ -0,0 +1,17 @@
+var api = "http://pool.kepl.org:1117";
+
+var poolHost = "pool.kepl.org";
+
+var irc = "irc.freenode.net/#keplpool";
+
+var email = "contact@kepl.org";
+
+var cryptonatorWidget = ["{symbol}-BTC", "{symbol}-USD", "{symbol}-EUR"];
+
+var easyminerDownload = "https://github.com/zone117x/cryptonote-easy-miner/releases/";
+
+var blockchainExplorer = "http://explorer.kepl.org/?hash={id}#blockchain_block";
+
+var transactionExplorer = "http://explorer.kepl.org/?hash={id}#blockchain_transaction";
+
+var themeCss = "themes/default-theme.css";
